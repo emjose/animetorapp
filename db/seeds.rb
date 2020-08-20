79b86e78c90b667de7894d919cd97cc45bd8507b
emjose <emjose@gmail.com>
2020-08-20 09:27:33 -0400

    characters and questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,79 @@
+#Avatar Characters
+cabbageman = Avatar.create(avatar_id: “The Cabbage Man! Also known as the cabbage merchant, you are constantly plagued by the misfortune of having your cabbage cart continually destroyed.”)
+
+sozin = Avatar.create(avatar_id: “Sozin, the intelligent yet ruthless Fire Lord and instigator of the Hundred Year War.”)
+
+ozai = Avatar.create(avatar_id: “Ozai, the strong and power-hungry ruler of the Fire Nation with an almost unmatchable mastery of bending fire.”)
+
+hama = Avatar.create(avatar_id: “Hama, a seemingly gentle and kind old woman, but you harbor a dark skill of bending blood to you will.”)
+
+momo = Avatar.create(avatar_id: “Momo, the winged lemur companion of Avatar Aang. You are curious, playful, intelligent, and most of all, loyal.”)
+
+appa = Avatar.create(avatar_id: “Appa, the loyal and trusted Sky bison of Avatar Aang. Despite numerous obstacles, you persevere and rise to the occasion.”)
+
+mai = Avatar.create(avatar_id: “Mai, a master markswoman of knives and arrows. You are outwardly stoic and brooding, but you are flowing with compassion and mercy.”)
+
+suki = Avatar.create(avatar_id: “Suki, a woman of action who excels in hand-to-hand combat. You are a fast learner and possess a strong sense of duty.”)
+
+june = Avatar.create(avatar_id: “June, an exceptional bounty hunter with a sarcastic wit. You have no allegiance to anyone, except for the highest bidder of your services.”)
+
+bumi = Avatar.create(avatar_id: “Bumi, the rambunctious son of Avatar Aang. You are at times reckless, but you always have the best interests of your loved ones.”)
+
+sokka = Avatar.create(avatar_id: “Sokka, the skeptical and abrasive brother of Katara. You learn to be flexible over time, and you have no shortage of loyalty.”
+
+combustionman = Avatar.create(avatar_id: “The Combustion Man, a masterful and silent fire-bending assassin with a unique ability to detonate large explosions from your third-eye tattoo.”)
+
+kuruk = Avatar.create(avatar_id: “Kuruk, the Water Tribe-born Avatar. At times you are your own worst enemy, but you are widely loved by your friends and family.
+
+tenzin = Avatar.create(avatar_id: “Tenzin, the youngest son of Avatar Aang. You are an airbending master and stern in nature, but a sense of humor does slip through at times.”
+
+jet = Avatar.create(avatar_id: “Jet, the young leader of the Earth Kingdom Freedom Fighters. Although you are fueled by revenge, you have a magnetic, charismatic personality.”)
+
+gyatso = Avatar.create(avatar_id: “Gyatso, the kind, wise mentor of Avatar Aang. You are excellent at airbending and you balance you wisdom with a great sense of humor.”)
+
+korra = Avatar.create(avatar_id: “Korra, the immediate successor of Avatar Aang. Extremelly powerful at bending all elements, you are fierce and possess strong loyalty towards your loved ones and duty as the Avatar.”)
+
+zuko = Avatar.create(avatar_id: “Zuko, the prodigal son of Fire Lord Ozai. Despite internal struggle and turmoil, you are driven by hope and achieve great wisdom.”)
+
+toph = Avatar.create(avatar_id: “Toph, a powerful, earthbending master. Your visual impairment is of no hindrance, and you are confident, honest, and full of no-nonsense.”)
+
+azula = Avatar.create(avatar_id: “Azula, a firebending prodigy and sister of Zuko. At times lacking empathy, you overcome your insecurities and discover a strong sense of self.”)
+
+roku = Avatar.create(avatar_id: “Roku, the wise Fire Nation-born Avatar. You are self-critica and regretfull, but you are warm-hearted, disciplined, and wise.”)
+
+kyoshi = Avatar.create(avatar_id: “Kyoshi, the Earth Kingdom-born Avatar. You are a merciless pursuer of justice, and you have a far-reaching legacy of fear and admiration.”)
+
+iroh = Avatar.create(avatar_id: “Iroh, a retired Fire Nation General and compassionate uncle of Zuko. You are wise, easygoing, and you live life to the fullest.”
+
+yangchen = Avatar.create(avatar_id: “Yangchen, the Air Nomad Avatar before Aang. You are an encouraging and empathetic leader, and you are known for your great compassion.”)
+
+katara = Avatar.create(avatar_id: “Katara, a waterbending master. You are compassionate, and your inner strength drives your determination to fight for your loved ones.”)
+
+aang = Avatar.create(avatar_id: “Aang, the Air Nomad Avatar. As a reluctant hero, you are somewhat naïve and carefree, but you rise to the duty as the Avatar to bring peace to the World of the Benders.”)
+
+wan = Avatar.create(avatar_id: “Wan, the very first Avatar. You are initially reckless and temperamental, but only because you feel that others are being treated unjustly. You are humble, brave, and achieve full mastery of Air, Water, Earth, and Fire!
+
+Avatar.destroy_all
+Quiz.destroy_all
+User.destroy_all
+Avatar.reset_pk_sequence
+Quiz.reset_pk_sequence
+User.reset_pk_sequence
+
+#Minor Characters as User instances, for method find other users?
+joodee = User.create(name: “Joo Dee”)
+princessyue = User.create(name: “Princess Yue”)
+grangran = User.create(name: “Gran-Gran”)
+auntwu = User.create(name: “Aunt Wu”)
+chiefhakoda = User.create(name: “Chief Hakoda”)
+arnook = User.create(name: “Arnook”)
+theboulder = User.create(name: “The Boulder”)
+haru = User.create(name: “Haru”)
+teo = User.create(name: “Teo”)
+onji = User.create(name: “On Ji”)
+
+
+
 #comment
 
 # Welcome to the World of Benders! Its been 100 years since the last Avatar has vanished.
